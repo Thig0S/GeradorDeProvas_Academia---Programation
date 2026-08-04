2e393551a7015128bd3ab44fc707b93582be7b9e
thig0S_ <thiagokovalski5@gmail.com>
2026-08-04 18:07:12 -0300

    add teste de Autenticacao de cadastro de usuario

---
+++ test/GeradorDeProvas.Test.E2E/AutenticacaoE2ETests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace GeradorDeProvas.Test.E2E;
+
+[TestClass]
+public sealed class AutenticacaoE2ETests : PageTest
+{
+    private string urlBase = "http://localhost:8001";
+
+    [TestMethod]
+    public async Task Deve_Exibir_TelaDeLogin_Para_UsuarioAnonimo()
+    {
+        //arrange
+
+        //act
+        await Page.GotoAsync($"{urlBase}");
+
+        //assert
+        await Expect(Page).ToHaveTitleAsync(new Regex("Entrar"));
+    }
+    [TestMethod]
+    public async Task Deve_RegistrarEAutenticar_Usuario()
+    {
+        //arrange
+        const string email = "novo.usuario@teste.local";
+        const string senha = "senha123!";
+
+        await Page.GotoAsync($"{urlBase}/Autenticacao/Registrar");
+
+        //act
+        await Page.GetByLabel("E-Mail").FillAsync(email);
+        //diz que precisar ser o campo exato
+        await Page.GetByLabel("Senha", new() { Exact = true }).FillAsync(senha);
+        await Page.GetByLabel("Confirmar Senha").FillAsync(senha);
+
+        //selecionar pelo papel do Botão com o nome Criar Conta
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Criar Conta" }).ClickAsync();
+
+        //assert
+        string rotaAbsoluta = new Uri(Page.Url).AbsolutePath;
+        Assert.AreEqual("/", rotaAbsoluta);
+    }
+}
